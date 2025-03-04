a3ffe7218c84361030179ca5ce9bfc4d4d2d2683
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-03-04 23:23:31 +0530

    Update QuestionController.cs

--- My_Project_dotNET/Controllers/QuestionController.cs
+++ My_Project_dotNET/Controllers/QuestionController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.SqlClient;
 using System.Data;
 using Microsoft.AspNetCore.Mvc;
+using My_Project_dotNET.Models;
 
 namespace My_Project_dotNET.Controllers
 {
@@ -25,11 +26,6 @@ namespace My_Project_dotNET.Controllers
             DataTable table = new DataTable();
             table.Load(reader);
             return View(table);
-        }
-
-        public IActionResult AddQuestion()
-        {
-            return View();
         }
 
         public IActionResult QuestionDelete(int QuestionID)
@@ -57,6 +53,145 @@ namespace My_Project_dotNET.Controllers
                 TempData["ErrorMessage"] = "An error occurred while deleting the Question: " + ex.Message;
                 return RedirectToAction("QuestionList");
             }
+        }
+
+        [HttpGet]
+        public IActionResult AddQuestion(int? QuestionID)
+        {
+            if (QuestionID == null)
+            {
+                var m = new QuestionModel
+                {
+                    Created = DateTime.Now
+                };
+                QuestionLevelDropDown();
+                UserDropDown();
+                return View(m);
+            }
+
+            string connectionString = this.configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "PR_MST_Question_SelectByPK";
+            command.Parameters.AddWithValue("@QuestionID", QuestionID);
+            //command.Parameters.AddWithValue("@UserID", CommonVariable.UserID());
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            QuestionModel model = new QuestionModel();
+
+            foreach (DataRow dataRow in table.Rows)
+            {
+                model.QuestionID = Convert.ToInt32(dataRow["QuestionID"]);
+                model.QuestionText = dataRow["QuestionText"].ToString();
+                model.QuestionLevelID = Convert.ToInt32(dataRow["QuestionLevelID"]);
+                model.OptionA = dataRow["OptionA"].ToString();
+                model.OptionB = dataRow["OptionB"].ToString();
+                model.OptionC = dataRow["OptionC"].ToString();
+                model.OptionD = dataRow["OptionD"].ToString();
+                model.CorrectOption = dataRow["CorrectOption"].ToString();
+                model.QuestionMarks = Convert.ToInt32(dataRow["QuestionMarks"]);
+                model.IsActive = Convert.ToBoolean(dataRow["IsActive"]);
+                model.UserID = Convert.ToInt32(dataRow["UserID"]);
+                model.Created = dataRow["Created"] != DBNull.Value ? Convert.ToDateTime(dataRow["Created"]) : DateTime.UtcNow;
+                model.Modified = dataRow["Modified"] != DBNull.Value ? Convert.ToDateTime(dataRow["Modified"]) : DateTime.UtcNow;
+            }
+
+            QuestionLevelDropDown();
+            UserDropDown();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult QuestionAddEdit(QuestionModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Console.WriteLine($"QuestionID: {model.QuestionID}"); // in update it is still zero
+
+                string connectionString = this.configuration.GetConnectionString("ConnectionString");
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+
+                if (model.QuestionID == 0)
+                {
+                    command.CommandText = "PR_MST_Question_Insert";
+                }
+                else
+                {
+                    command.CommandText = "PR_MST_Question_Update";
+                    command.Parameters.Add("@QuestionID", SqlDbType.Int).Value = model.QuestionID;
+                }
+                command.Parameters.Add("@QuestionText", SqlDbType.VarChar).Value = model.QuestionText;
+                command.Parameters.Add("@OptionA", SqlDbType.VarChar).Value = model.OptionA;
+                command.Parameters.Add("@OptionB", SqlDbType.VarChar).Value = model.OptionB;
+                command.Parameters.Add("@OptionC", SqlDbType.VarChar).Value = model.OptionC;
+                command.Parameters.Add("@OptionD", SqlDbType.VarChar).Value = model.OptionD;
+
+                command.Parameters.Add("@QuestionLevelID", SqlDbType.Int).Value = model.QuestionLevelID;
+                command.Parameters.Add("@CorrectOption", SqlDbType.VarChar).Value = model.CorrectOption;
+                command.Parameters.Add("@QuestionMarks", SqlDbType.Int).Value = model.QuestionMarks;
+                command.Parameters.Add("@UserID", SqlDbType.Int).Value = model.UserID;
+
+                command.ExecuteNonQuery();
+                return RedirectToAction("QuestionList");
+            }
+
+            QuestionLevelDropDown();
+            UserDropDown();
+            return RedirectToAction("QuestionList");
+        }
+
+        [HttpGet]
+        public void UserDropDown()
+        {
+            string connectionString = this.configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command2 = connection.CreateCommand();
+            command2.CommandType = System.Data.CommandType.StoredProcedure;
+            command2.CommandText = "PR_MST_User_Fill_Dropdown";
+            //command2.Parameters.Add("@UserID", SqlDbType.Int).Value = CommonVariable.UserID();
+            SqlDataReader reader2 = command2.ExecuteReader();
+            DataTable dataTable2 = new DataTable();
+            dataTable2.Load(reader2);
+            List<UserDropDownModel> UserList = new List<UserDropDownModel>();
+            foreach (DataRow data in dataTable2.Rows)
+            {
+                UserDropDownModel model = new UserDropDownModel();
+                model.UserID = Convert.ToInt32(data["UserID"]);
+                model.UserName = data["UserName"].ToString();
+                UserList.Add(model);
+            }
+            ViewBag.UserList = UserList;
+        }
+
+        [HttpGet]
+        public void QuestionLevelDropDown()
+        {
+            string connectionString = this.configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command2 = connection.CreateCommand();
+            command2.CommandType = System.Data.CommandType.StoredProcedure;
+            command2.CommandText = "PR_MST_QuestionLevel_Fill_Dropdown";
+            //command2.Parameters.Add("@UserID", SqlDbType.Int).Value = CommonVariable.UserID();
+            SqlDataReader reader2 = command2.ExecuteReader();
+            DataTable dataTable2 = new DataTable();
+            dataTable2.Load(reader2);
+            List<QuestionLevelDropDownModel> QuestionLevelList = new List<QuestionLevelDropDownModel>();
+            foreach (DataRow data in dataTable2.Rows)
+            {
+                QuestionLevelDropDownModel model = new QuestionLevelDropDownModel();
+                model.QuestionLevelID = Convert.ToInt32(data["QuestionLevelID"]);
+                model.QuestionLevel = data["QuestionLevel"].ToString();
+                QuestionLevelList.Add(model);
+            }
+            ViewBag.QuestionLevelList = QuestionLevelList;
         }
     }
 }
