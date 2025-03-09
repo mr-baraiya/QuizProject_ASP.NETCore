787cf2b420fba31d4b7f99b4740855743b10d007
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-03-09 18:04:54 +0530

    Update HomeController.cs

--- My_Project_dotNET/Controllers/HomeController.cs
+++ My_Project_dotNET/Controllers/HomeController.cs
@@ -1,20 +1,61 @@
+using System.Data.SqlClient;
+using System.Data;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using My_Project_dotNET.Models;
+using Microsoft.Extensions.Logging;
 
 namespace My_Project_dotNET.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private IConfiguration configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IConfiguration _configuration)
         {
-            _logger = logger;
+            this.configuration = _configuration;
         }
 
+
+        [HttpGet]
+        [Route("Dashboard")]
         public IActionResult Index()
         {
+            string connectionString = this.configuration.GetConnectionString("ConnectionString");
+
+            // Create DataTables for quizzes and questions
+            DataTable quizTable = new DataTable();
+            DataTable questionTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Get recent quizzes
+                using (SqlCommand quizCommand = new SqlCommand("PR_Quiz_Get_Recent_Quizzes", connection))
+                {
+                    quizCommand.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader quizReader = quizCommand.ExecuteReader())
+                    {
+                        quizTable.Load(quizReader);
+                    }
+                }
+
+                // Get recent questions
+                using (SqlCommand questionCommand = new SqlCommand("PR_Question_Get_Recent_Questions", connection))
+                {
+                    questionCommand.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader questionReader = questionCommand.ExecuteReader())
+                    {
+                        questionTable.Load(questionReader);
+                    }
+                }
+            }
+
+            // Pass both tables to ViewData
+            ViewData["RecentQuizzes"] = quizTable;
+            ViewData["RecentQuestions"] = questionTable;
+
             return View();
         }
 
@@ -22,7 +63,19 @@ namespace My_Project_dotNET.Controllers
         {
             return View();
         }
-
+        public IActionResult QuizList()
+        {
+            string connectionString = this.configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "PR_MST_Quiz_SelectAll";
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            return View(table);
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
