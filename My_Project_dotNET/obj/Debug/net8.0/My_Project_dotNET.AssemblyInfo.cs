48335f22ba7fe97d249dcba6be9c424fe91017e6
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-03-12 08:10:23 +0530

    12-03-2025 commit

--- My_Project_dotNET/obj/Debug/net8.0/My_Project_dotNET.AssemblyInfo.cs
+++ My_Project_dotNET/obj/Debug/net8.0/My_Project_dotNET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My_Project_dotNET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45cb93befc7bd3678b2dd1b60d0cbc0e5632a9b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace80b6fa70077e1b7d64d6ac1c6aaf6213fa9f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("My_Project_dotNET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My_Project_dotNET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
