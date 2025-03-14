dc18a0a48d232b52aedeed166e9f509b31e6abbd
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-03-14 20:37:22 +0530

    Update My_Project_dotNET.AssemblyInfo.cs

--- My_Project_dotNET/obj/Debug/net8.0/My_Project_dotNET.AssemblyInfo.cs
+++ My_Project_dotNET/obj/Debug/net8.0/My_Project_dotNET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My_Project_dotNET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48335f22ba7fe97d249dcba6be9c424fe91017e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed5d2cad7bcfd52b1da9f093f98f1f3deca5850f")]
 [assembly: System.Reflection.AssemblyProductAttribute("My_Project_dotNET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My_Project_dotNET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
