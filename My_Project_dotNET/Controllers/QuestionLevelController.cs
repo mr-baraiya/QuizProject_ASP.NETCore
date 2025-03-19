779175b9d6b0eb40488c8e7f8c8e378c5479be92
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-03-19 22:16:05 +0530

    Update QuestionLevelController.cs

--- My_Project_dotNET/Controllers/QuestionLevelController.cs
+++ My_Project_dotNET/Controllers/QuestionLevelController.cs
@@ -16,25 +16,31 @@ namespace My_Project_dotNET.Controllers
             this.configuration = _configuration;
         }
 
+        #region QuestionLevelList
         public IActionResult QuestionLevelList()
         {
-            string connectionString = this.configuration.GetConnectionString("ConnectionString");
-            SqlConnection connection = new SqlConnection(connectionString);
-            connection.Open();
-            SqlCommand command = connection.CreateCommand();
-            command.CommandType = CommandType.StoredProcedure;
-            command.CommandText = "PR_MST_QuestionLevel_SelectAll";
-            SqlDataReader reader = command.ExecuteReader();
-            DataTable table = new DataTable();
-            table.Load(reader);
-            return View(table);
+            try
+            {
+                string connectionString = this.configuration.GetConnectionString("ConnectionString");
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "PR_MST_QuestionLevel_SelectAll";
+                SqlDataReader reader = command.ExecuteReader();
+                DataTable table = new DataTable();
+                table.Load(reader);
+                return View(table);
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "An error occurred while fetching QuestionLevels: " + ex.Message;
+                return RedirectToAction("QuestionLevelList");
+            }
         }
+        #endregion
 
-        public IActionResult AddQuestionLevel()
-        {
-            return View();
-        }
-
+        #region QuestionLevelDelete
         public IActionResult QuestionLevelDelete(int QuestionLevelID)
         {
             try
@@ -48,7 +54,6 @@ namespace My_Project_dotNET.Controllers
                     command.CommandText = "PR_MST_QuestionLevel_Delete";
                     command.Parameters.Add("@questionLevelID", SqlDbType.Int).Value = QuestionLevelID;
 
-
                     command.ExecuteNonQuery();
                 }
 
@@ -61,123 +66,148 @@ namespace My_Project_dotNET.Controllers
                 return RedirectToAction("QuestionLevelList");
             }
         }
+        #endregion
 
+        #region AddQuestionLevel
         [HttpGet]
-        public IActionResult AddQuestionLevel(int? QuestionLevelID)
+        public IActionResult AddQuestionLevel(int QuestionLevelID)
         {
-            if (QuestionLevelID == null)
+            try
             {
-                var m = new QuestionLevelModel
+                if (QuestionLevelID == null)
                 {
-                    Created = DateTime.Now
-                };
-                return View(m);
-            }
+                    var m = new QuestionLevelModel
+                    {
+                        Created = DateTime.Now
+                    };
+                    return View(m);
+                }
 
-            string connectionString = this.configuration.GetConnectionString("ConnectionString");
-            SqlConnection connection = new SqlConnection(connectionString);
-            connection.Open();
-            SqlCommand command = connection.CreateCommand();
-            command.CommandType = CommandType.StoredProcedure;
-            command.CommandText = "PR_MST_QuestionLevel_SelectByPK";
-            command.Parameters.AddWithValue("@QuestionLevelID", QuestionLevelID);
-            //command.Parameters.AddWithValue("@UserID", CommonVariable.UserID());
-            SqlDataReader reader = command.ExecuteReader();
-            DataTable table = new DataTable();
-            table.Load(reader);
-            QuestionLevelModel model = new QuestionLevelModel();
-
-            foreach (DataRow dataRow in table.Rows)
-            {
-                model.QuestionLevelID = Convert.ToInt32(dataRow["QuestionLevelID"]);
-                model.QuestionLevel = dataRow["QuestionLevel"].ToString();
-                model.UserID = Convert.ToInt32(CommonVariables.UserID());
-                model.Created = dataRow["Created"] != DBNull.Value ? Convert.ToDateTime(dataRow["Created"]) : DateTime.UtcNow;
-                model.Modified = dataRow["Modified"] != DBNull.Value ? Convert.ToDateTime(dataRow["Modified"]) : DateTime.UtcNow;
-            }
-            return View(model);
-        }
-
-        [HttpPost]
-        public IActionResult QuestionLevelAddEdit(QuestionLevelModel model)
-        {
-            if (ModelState.IsValid)
-            {
                 string connectionString = this.configuration.GetConnectionString("ConnectionString");
                 SqlConnection connection = new SqlConnection(connectionString);
                 connection.Open();
                 SqlCommand command = connection.CreateCommand();
                 command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "PR_MST_QuestionLevel_SelectByPK";
+                command.Parameters.AddWithValue("@QuestionLevelID", QuestionLevelID);
+                SqlDataReader reader = command.ExecuteReader();
+                DataTable table = new DataTable();
+                table.Load(reader);
+                QuestionLevelModel model = new QuestionLevelModel();
 
-                if (model.QuestionLevelID == 0)
+                foreach (DataRow dataRow in table.Rows)
                 {
-                    command.CommandText = "PR_MST_QuestionLevel_Insert";
+                    model.QuestionLevelID = Convert.ToInt32(dataRow["QuestionLevelID"]);
+                    model.QuestionLevel = dataRow["QuestionLevel"].ToString();
+                    model.UserID = Convert.ToInt32(model.UserID);
                 }
-                else
-                {
-                    command.CommandText = "PR_MST_QuestionLevel_Update";
-                    command.Parameters.Add("@QuestionLevelID", SqlDbType.Int).Value = model.QuestionLevelID;
-                }
-                command.Parameters.Add("@QuestionLevel", SqlDbType.VarChar).Value = model.QuestionLevel;
-                command.Parameters.Add("@UserId", SqlDbType.Int).Value = CommonVariables.UserID();
-                command.ExecuteNonQuery();
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "An error occurred while fetching the QuestionLevel: " + ex.Message;
                 return RedirectToAction("QuestionLevelList");
             }
-            return RedirectToAction("QuestionLevelList");
         }
+        #endregion
 
+        #region QuestionLevelAddEdit
+        [HttpPost]
+        public IActionResult QuestionLevelAddEdit(QuestionLevelModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string connectionString = this.configuration.GetConnectionString("ConnectionString");
+                    SqlConnection connection = new SqlConnection(connectionString);
+                    connection.Open();
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    if (model.QuestionLevelID <= 0)
+                    {
+                        command.CommandText = "PR_MST_QuestionLevel_Insert";
+                    }
+                    else
+                    {
+                        command.CommandText = "PR_MST_QuestionLevel_Update";
+                        command.Parameters.Add("@QuestionLevelID", SqlDbType.Int).Value = model.QuestionLevelID;
+                    }
+                    command.Parameters.Add("@QuestionLevel", SqlDbType.VarChar).Value = model.QuestionLevel;
+                    command.Parameters.Add("@UserID", SqlDbType.Int).Value = model.UserID;
+                    command.ExecuteNonQuery();
+                }
+                return RedirectToAction("QuestionLevelList");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "An error occurred while saving the QuestionLevel: " + ex.Message;
+                return RedirectToAction("QuestionLevelList");
+            }
+        }
+        #endregion
+
+        #region ExportQuestionLevelsToExcel
         [HttpGet]
         public IActionResult ExportQuestionLevelsToExcel()
         {
-            string connectionString = configuration.GetConnectionString("ConnectionString");
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            try
             {
-                sqlConnection.Open();
-
-                using (SqlCommand sqlCommand = sqlConnection.CreateCommand())
+                string connectionString = configuration.GetConnectionString("ConnectionString");
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
                 {
-                    sqlCommand.CommandType = CommandType.StoredProcedure;
-                    sqlCommand.CommandText = "PR_MST_QuestionLevel_SelectAll"; // Replace with the stored procedure name
+                    sqlConnection.Open();
 
-                    using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                    using (SqlCommand sqlCommand = sqlConnection.CreateCommand())
                     {
-                        DataTable data = new DataTable();
-                        data.Load(sqlDataReader);
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
+                        sqlCommand.CommandText = "PR_MST_QuestionLevel_SelectAll";
 
-                        using (var package = new ExcelPackage())
+                        using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
                         {
-                            var worksheet = package.Workbook.Worksheets.Add("QuestionLevels");
+                            DataTable data = new DataTable();
+                            data.Load(sqlDataReader);
 
-                            // Add headers
-                            worksheet.Cells[1, 1].Value = "QuestionLevelID";
-                            worksheet.Cells[1, 2].Value = "QuestionLevel";
-                            worksheet.Cells[1, 3].Value = "UserID";
-                            worksheet.Cells[1, 4].Value = "Created";
-                            worksheet.Cells[1, 5].Value = "Modified";
-
-                            // Add data
-                            int row = 2;
-                            foreach (DataRow item in data.Rows)
+                            using (var package = new ExcelPackage())
                             {
-                                worksheet.Cells[row, 1].Value = item["QuestionLevelID"];
-                                worksheet.Cells[row, 2].Value = item["QuestionLevel"];
-                                worksheet.Cells[row, 3].Value = item["UserID"];
-                                worksheet.Cells[row, 4].Value = item["Created"] != DBNull.Value ? Convert.ToDateTime(item["Created"]) : (DateTime?)null;
-                                worksheet.Cells[row, 5].Value = item["Modified"] != DBNull.Value ? Convert.ToDateTime(item["Modified"]) : (DateTime?)null;
-                                row++;
+                                var worksheet = package.Workbook.Worksheets.Add("QuestionLevels");
+
+                                worksheet.Cells[1, 1].Value = "QuestionLevelID";
+                                worksheet.Cells[1, 2].Value = "QuestionLevel";
+                                worksheet.Cells[1, 3].Value = "UserID";
+                                worksheet.Cells[1, 4].Value = "Created";
+                                worksheet.Cells[1, 5].Value = "Modified";
+
+                                int row = 2;
+                                foreach (DataRow item in data.Rows)
+                                {
+                                    worksheet.Cells[row, 1].Value = item["QuestionLevelID"];
+                                    worksheet.Cells[row, 2].Value = item["QuestionLevel"];
+                                    worksheet.Cells[row, 3].Value = item["UserID"];
+                                    worksheet.Cells[row, 4].Value = item["Created"] != DBNull.Value ? Convert.ToDateTime(item["Created"]) : (DateTime?)null;
+                                    worksheet.Cells[row, 5].Value = item["Modified"] != DBNull.Value ? Convert.ToDateTime(item["Modified"]) : (DateTime?)null;
+                                    row++;
+                                }
+
+                                var stream = new MemoryStream();
+                                package.SaveAs(stream);
+                                stream.Position = 0;
+
+                                string excelName = $"QuestionLevels-{DateTime.Now:yyyyMMddHHmmss}.xlsx";
+                                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName);
                             }
-
-                            var stream = new MemoryStream();
-                            package.SaveAs(stream);
-                            stream.Position = 0;
-
-                            string excelName = $"QuestionLevels-{DateTime.Now:yyyyMMddHHmmss}.xlsx";
-                            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName);
                         }
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "An error occurred while exporting QuestionLevels: " + ex.Message;
+                return RedirectToAction("QuestionLevelList");
+            }
         }
+        #endregion
 
     }
 }
