e4948e100f51e713b2df33d5d5e19ab8d39f5758
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-03-20 15:35:45 +0530

    Update HomeController.cs

--- My_Project_dotNET/Controllers/HomeController.cs
+++ My_Project_dotNET/Controllers/HomeController.cs
@@ -17,56 +17,88 @@ namespace My_Project_dotNET.Controllers
             this.configuration = _configuration;
         }
 
-
+        #region Dashboard
         [HttpGet]
         [Route("Dashboard")]
         public IActionResult Index()
         {
-            string connectionString = this.configuration.GetConnectionString("ConnectionString");
-
-            // Create DataTables for quizzes and questions
-            DataTable quizTable = new DataTable();
-            DataTable questionTable = new DataTable();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            try
             {
-                connection.Open();
+                string connectionString = this.configuration.GetConnectionString("ConnectionString");
 
-                // Get recent quizzes
-                using (SqlCommand quizCommand = new SqlCommand("PR_Quiz_Get_Recent_Quizzes", connection))
+                // Create DataTables for quizzes and questions
+
+                DataTable quizTable = new DataTable();
+                DataTable questionTable = new DataTable();
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    quizCommand.CommandType = CommandType.StoredProcedure;
-                    using (SqlDataReader quizReader = quizCommand.ExecuteReader())
+                    connection.Open();
+
+
+                    // Get recent quizzes
+                    using (SqlCommand quizCommand = new SqlCommand("PR_Quiz_Get_Recent_Quizzes", connection))
                     {
-                        quizTable.Load(quizReader);
+                        quizCommand.CommandType = CommandType.StoredProcedure;
+                        using (SqlDataReader quizReader = quizCommand.ExecuteReader())
+                        {
+                            quizTable.Load(quizReader);
+                        }
+                    }
+
+                    // Get recent questions
+                    using (SqlCommand questionCommand = new SqlCommand("PR_Question_Get_Recent_Questions", connection))
+                    {
+                        questionCommand.CommandType = CommandType.StoredProcedure;
+                        using (SqlDataReader questionReader = questionCommand.ExecuteReader())
+                        {
+                            questionTable.Load(questionReader);
+                        }
                     }
                 }
 
-                // Get recent questions
-                using (SqlCommand questionCommand = new SqlCommand("PR_Question_Get_Recent_Questions", connection))
-                {
-                    questionCommand.CommandType = CommandType.StoredProcedure;
-                    using (SqlDataReader questionReader = questionCommand.ExecuteReader())
-                    {
-                        questionTable.Load(questionReader);
-                    }
-                }
+                // Pass both tables to ViewData
+
+                ViewData["RecentQuizzes"] = quizTable;
+                ViewData["RecentQuestions"] = questionTable;
+
+                return View();
             }
-
-            // Pass both tables to ViewData
-            ViewData["RecentQuizzes"] = quizTable;
-            ViewData["RecentQuestions"] = questionTable;
-
-            return View();
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "An error occurred while loading the dashboard: " + ex.Message;
+                return RedirectToAction("Index", "Home");
+            }
         }
+        #endregion
 
+        #region Privacy & Error
         public IActionResult Privacy()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "An error occurred while loading the Privacy page: " + ex.Message;
+                return RedirectToAction("Index", "Home");
+            }
         }
+
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            try
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "An error occurred while loading the Error page: " + ex.Message;
+                return RedirectToAction("Index", "Home");
+            }
         }
+        #endregion
+
     }
 }
